47958169ac9afee81627d3b87483647e199aaf0d
Mukesh A <amukesh.mk@gmail.com>
2020-02-07 23:09:11 +0000

    added new functions to compare and retrieve data based on the hash value of the data-set

--- sklearn/fml/encryption/fml_hash.py
+++ sklearn/fml/encryption/fml_hash.py
@@ -5,11 +5,17 @@ class FMLHash:
         return
     
     def hashVal(self, value):
+        """
+        converts the value to a byte object and then hashes it
+        """
         m = hashlib.sha256()
         m.update(value.encode())
         return m.digest()
         
     def hashValAndReturnString(self, value):
+        """
+        converts the value to a byte object and then hashes it and converts it to string
+        """
         m = hashlib.sha1()
         m.update(value.encode())
         return m.digest().decode("unicode_escape")
